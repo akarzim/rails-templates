6be14726fcdac3f01dd9bbe9f3c271d5597e66c4
François Vantomme <akarzim@gmail.com>
2015-06-10 11:18:52 +0200

    Update locale partial by adding a route scope

--- _locale.rb
+++ _locale.rb
@@ -1,6 +1,13 @@
 locales = (ENV['LOCALES'] || ask('Which locales do you want to use (separate with commas if more)? [en]')).split(/[,\s]+/)
 locales = [locales].flatten(1).compact
 
+insert_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do
+  <<-CODE
+  scope "(:locale), locale: /#{locales.join('|')}/ do
+  end
+  CODE
+end
+
 locales.each do |locale|
   append_to_file 'app/views/layouts/_header.slim', <<-CODE
       = link_to('#{locale.upcase}', params.merge(locale: :#{locale.downcase}), class: 'locale')
